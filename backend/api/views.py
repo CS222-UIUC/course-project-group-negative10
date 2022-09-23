541d8b7a772c3a94e71c06fa23af45cbbec8522d
Batuhan Usluel <batuhan.usluel@gmail.com>
2022-09-23 02:15:54 -0500

    Add a basic google play scraper to app

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,14 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from google_play_scraper import app
 
 class SimpleAPIView(APIView):
     def get(self, request):
         text = request.query_params.get('text', '')
-        return Response({"text": text.upper()}, status=status.HTTP_200_OK)
+        result = app(
+            text,
+            lang='en', # defaults to 'en'
+            country='us' # defaults to 'us'
+        )
+        return Response({"text": str(result)}, status=status.HTTP_200_OK)
