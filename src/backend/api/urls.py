921eaafaeb08657da3ab27d19fe67099d62a2264
Charleston Wang <wangcharles10@gmail.com>
2022-12-08 20:06:48 -0600

    app store scraper

--- src/backend/api/urls.py
+++ src/backend/api/urls.py
@@ -7,5 +7,5 @@ from .views import NLP
 urlpatterns = [
     path('test', SimpleAPIView.as_view(), name='test_api_view'),
     path('getReviews', getReviews.as_view(), name='getReviews_view'),
-    path('NLP', NLP.as_view(), name='NLP')
+    path('NLP', NLP.as_view(), name='NLP_view')
 ]
--- src/backend/api/views.py
+++ src/backend/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from google_play_scraper import app, reviews, Sort, reviews_all
+from app_store_scraper import AppStore
 from collections import defaultdict
 import datetime
 import json
@@ -24,30 +25,49 @@ class SimpleAPIView(APIView):
 
 class getReviews(APIView):
     def get(self, request):
-        app_name = request.query_params.get('text', '')
-        result, continuation_token = reviews(
-            app_name,
-            # lang='en', # defaults to 'en'
-            # country='us', # defaults to 'us'
-            # sort=Sort.NEWEST, # defaults to Sort.NEWEST
-            count=300, # defaults to 100
-            # filter_score_with=5 # defaults to None(means all score)
-        )
-        #nlp = spacy.load("en_core_web_trf")
-        review_data = defaultdict(list)
-        for review in reversed(result):
-            day = review['at'].strftime('%Y-%m-%d')
-            review_data[day].append(review['score'])
+        store = request.query_params.get('store', '') 
+        appname = request.query_params.get('text', '')
+        if store == "Play Store":
+            result, continuation_token = reviews(
+                appname,
+                # lang='en', # defaults to 'en'
+                # country='us', # defaults to 'us'
+                # sort=Sort.NEWEST, # defaults to Sort.NEWEST
+                count=300, # defaults to 100
+                # filter_score_with=5 # defaults to None(means all score)
+            )
+            review_data = defaultdict(list)
+            for review in reversed(result):
+                day = review['at'].strftime('%Y-%m-%d')
+                review_data[day].append(review['score'])
 
-        review_data_average = {key:sum(values)/len(values) for key, values in review_data.items()}
+            review_data_average = {key:sum(values)/len(values) for key, values in review_data.items()}
 
-        review_data_average_list = []
-        for key, value in review_data_average.items():
-            review_data_average_list.append({'date': key, 'rating': round(value, 2)})
+            review_data_average_list = []
+            for key, value in review_data_average.items():
+                review_data_average_list.append({'date': key, 'rating': round(value, 2)})
+            
+            final_reviews = {'reviews': review_data_average_list}
+            return Response({"text": json.dumps(final_reviews)}, status=status.HTTP_200_OK)
+        elif store == "App Store":
+            app = AppStore(country="us",app_name=appname)
+            app.review(how_many=300)
+            review_data = defaultdict(list)
+            for review in reversed(app.reviews):
+                day = review['at'].strftime('%Y-%m-%d')
+                review_data[day].append(review['score'])
+            
+            review_data_average = {key:sum(values)/len(values) for key, values in review_data.items()}
+
+            review_data_average_list = []
+            for key, value in review_data_average.items():
+                review_data_average_list.append({'date': key, 'rating': round(value, 2)})
+            
+            final_reviews = {'reviews': review_data_average_list}
+            return Response({"text": json.dumps(final_reviews)}, status=status.HTTP_200_OK)
         
-        final_reviews = {'reviews': review_data_average_list}
-        return Response({"text": json.dumps(final_reviews)}, status=status.HTTP_200_OK)
-
+        
+        
 class NLP(APIView):
     def get(self, request):
             app_name = request.query_params.get('text', '')
@@ -84,3 +104,6 @@ class NLP(APIView):
             final_sentiments = {'sentiments': sentiment_data_average_list}
             print(final_sentiments)
             return Response({"text": json.dumps(final_sentiments)}, status=status.HTTP_200_OK)
+
+
+
