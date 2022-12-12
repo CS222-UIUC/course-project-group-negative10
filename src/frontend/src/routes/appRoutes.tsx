4699acf9c505d17253863b89da8c283b43fe3da3
Batuhan Usluel <batuhan.usluel@gmail.com>
2022-12-12 17:55:44 -0600

    Remove Downloads and Settings page

--- src/frontend/src/routes/appRoutes.tsx
+++ src/frontend/src/routes/appRoutes.tsx
@@ -3,14 +3,12 @@ import HomePage from "../pages/home/HomePage";
 import { RouteType } from "./config";
 import ReviewsOverTime from "../pages/reviews/ReviewsOverTime";
 import ReviewBrowser from "../pages/reviews/ReviewBrowser";
-import SettingsPage from "../pages/settings/SettingsPage";
 import SentimentAnalysis from "../pages/reviews/SentimentAnalysis";
 import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
 import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
 import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 import AppDetails from "../pages/myapps/AppDetails";
-import DownloadsPage from "../pages/downloads/DownloadsPage";
 
 const appRoutes: RouteType[] = [
   {
@@ -61,24 +59,6 @@ const appRoutes: RouteType[] = [
         }
       }
     ]
-  },
-  {
-    path: "/Downloads",
-    element: <DownloadsPage />,
-    state: "DownloadsPage",
-    sidebarProps: {
-      displayText: "Downloads",
-      icon: <ArticleOutlinedIcon />
-    }
-  },
-  {
-    path: "/Settings",
-    element: <SettingsPage />,
-    state: "settings",
-    sidebarProps: {
-      displayText: "Settings",
-      icon: <FormatListBulletedOutlinedIcon />
-    }
   }
 ];
 
